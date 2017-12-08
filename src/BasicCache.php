102eff0ccf34d165b574ee5a30d3f5a7679e3132
Arda Gedik <ardagedikk@hotmail.com>
2017-12-08 12:36:13 +0300

    Constructor method written

--- src/BasicCache.php
+++ src/BasicCache.php
@@ -25,6 +25,22 @@ class BasicCache{
 	// The path to the cached file
 	public $cachedFilePath;
 
+  // Constructor
+	public function __construct($options = []){
+
+		// Options
+		$this->path  		 = $options['path'] ?? 'cache/';
+		$this->expire 	 = $options['expire'] ?? 60;
+		$this->extension = $options['extension'] ?? '.html';
+
+		$this->fileName  = explode('.', basename($_SERVER['SCRIPT_NAME']))[0];
+		$this->cachedFileName = md5($this->fileName).$this->extension;
+		$this->cachedFilePath = $this->path.$this->cachedFileName;
+
+		// Create if directory does not exist
+		if( ! is_dir($this->path)) mkdir($this->path, 0755);
+	}
+
 }
 
 ?>
