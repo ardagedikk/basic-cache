c6b06dd0e6ad0eb07fa6d7d3e14793513bf8672c
Arda Gedik <ardagedikk@hotmail.com>
2017-12-08 12:34:27 +0300

    Class variables created

--- src/BasicCache.php
+++ src/BasicCache.php
@@ -4,7 +4,26 @@ namespace ArdaGedik;
 
 class BasicCache{
 
+  // Location of cache files
+	public $path;
 
+	// Cache expiration time
+	public $expire;
+
+	// Last modified date of cache file
+	public $modifiedTime;
+
+	// The name of the file that the cache is running on
+	public $fileName;
+
+	// The name of the cached file
+	public $cachedFileName;
+
+	// The extension of the cached file
+	public $cachedFileExt;
+
+	// The path to the cached file
+	public $cachedFilePath;
 
 }
 
